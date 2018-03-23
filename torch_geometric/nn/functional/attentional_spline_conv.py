9fbf915a66471d50d30850741581acb3d89e9090
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-23 16:12:43 +0100

    sample from bernoulli

--- torch_geometric/nn/functional/attentional_spline_conv.py
+++ torch_geometric/nn/functional/attentional_spline_conv.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
+from torch.distributions import Bernoulli
 from torch_spline_conv.functions.spline_weighting import spline_weighting
 
 
@@ -31,8 +32,8 @@ def attentional_spline_conv(x, edge_index, pseudo, weight, kernel_size,
     alpha /= alpha.new(n).fill_(0).scatter_add_(0, Variable(row), alpha)[row]
 
     # Apply attention.
-    alpha_dropped = F.dropout(alpha, p=dropout, training=True)
-    x_col = alpha_dropped.unsqueeze(-1) * x_col
+    alpha = F.dropout(alpha, p=dropout, training=True)
+    x_col = alpha.unsqueeze(-1) * x_col
 
     # Sum up neighborhoods.
     var_row = Variable(row.view(e, 1).expand(e, m_out))
@@ -41,5 +42,8 @@ def attentional_spline_conv(x, edge_index, pseudo, weight, kernel_size,
     # Add bias (if wished).
     if bias is not None:
         output += bias
+
+    # Sample from Bernoulli distribution.
+    # z = torch.bernoulli(alpha.data * (1 - dropout))
 
     return output
